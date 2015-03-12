d56e40ce43a5f7666ea0f8571068412f2ea9d410
igorsieradzki <sieradzki.igor@gmail.com>
2015-03-12 20:27:46 +0100

    testing refactor

--- fit_models.py
+++ fit_models.py
@@ -1,79 +1,136 @@
-from fit_r2elm import fit_r2elm_on_dataset, fit_r2elm_grid
-from fit_r2svm import fit_r2svm_on_dataset, fit_r2svm_grid
-from fit_elms import fit_elm_on_dataset, fit_elm_grid
-from fit_svms import fit_svc_on_dataset, fit_svc_grid
-from models import R2ELMLearner, R2SVMLearner, ELM
-from sklearn.svm import SVC
-from sklearn.cross_validation import cross_val_score
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import StratifiedKFold, cross_val_score
+from sklearn.metrics import accuracy_score
 from data_api import shuffle_data
 import time
-from misc.experiment_utils import get_exp_logger
 import numpy as np
 
-def fit_model_on_dataset(name, data, params):
 
-    E_grid, E_detailed = None, None
+def grid_search(model, data, param_grid, logger=None, scoring='accuracy', store_clf=False, n_jobs=8,
+                seed=None, more=False, n_folds=5, verbose=0):
+    """
+    :param model:       initialized model
+    :param data:        dict-like with data, targetm name fields
+    :param param_grid:  params for grid search
+    :param logger:      logger class
+    :param scoring:     scoring function, string or callable
+    :param store_clf:   save best classifier in monitors
+    :param n_jobs:      number of jobs to run on
+    :param seed:        seed for shuffling data
+    :param more:        save additional info in monitors
+    :param n_folds:     number of folds
+    :return:            dict with config, results and monitors fields
+    """
 
-    if name == 'r2svm':
-        E_grid, E_detailed = fit_r2svm_on_dataset(data,
-                                                  param_grid_in=params['model'],
-                                                  grid_config_in=params['grid'],
-                                                  fold_config_in=params['fold'],
-                                                  to_file=True)
-    elif name == 'r2elm':
-        E_grid, E_detailed = fit_r2elm_on_dataset(data,
-                                                  param_grid_in=params['model'],
-                                                  grid_config_in=params['grid'],
-                                                  fold_config_in=params['fold'],
-                                                  to_file=True)
-    elif name[:3] == 'elm':
-        E_grid, E_detailed = fit_elm_on_dataset(data,
-                                                param_grid_in=params['model'],
-                                                grid_config_in=params['grid'],
-                                                fold_config_in=params['fold'],
-                                                to_file=True)
-    elif name == 'svm' :
-        E_grid, E_detailed = fit_svc_on_dataset(data,
-                                                param_grid_in=params['model'],
-                                                grid_config_in=params['grid'],
-                                                fold_config_in=params['fold'],
-                                                to_file=True)
+    assert hasattr(data, 'name')
+    assert hasattr(data, 'data')
+    assert hasattr(data, 'target')
 
-    return E_grid, E_detailed
+    results = {}
+    monitors = {}
+    config = {}
+    experiment = {"config": config, "results": results, "monitors": monitors}
 
-def grid_model_on_dataset(name, data, params):
+    config['n_dim'] = data.n_dim
+    config['n_class'] = data.n_class
+    config['data_name'] = data.name
+    config['scoring'] = scoring
+    config['store_clf'] = store_clf
+    config['n_jobs'] = n_jobs
+    config['seed'] = seed
+    config['more'] = more
+    config['n_folds'] = n_folds
 
-    E_grid, E_detailed = None, None
-    config = params['grid']
-    config['param_grid'] = params['model']
-    config['experiment_name'] = name + "_grid_on_" + data.name + '_' + str(time.time())
-    logger = get_exp_logger(config, to_file=True)
+    X = data.data
+    Y = data.target
 
-    if name == 'r2svm':
-        E_grid = fit_r2svm_grid(data, config, logger)
-    elif name == 'r2elm':
-        E_grid = fit_r2elm_grid(data,config, logger)
-    elif name[:3] == 'elm':
-        E_grid= fit_elm_grid(data, config, logger)
-    elif name == 'svm' :
-        E_grid = fit_svc_grid(data, config, logger)
+    cv = StratifiedKFold(y=Y, n_folds=n_folds, shuffle=True, random_state=seed)
+    cv_grid = GridSearchCV(model, param_grid=param_grid, scoring=scoring, n_jobs=n_jobs, cv=cv, verbose=verbose)
 
-    return E_grid
+    start_time = time.time()
+    cv_grid.fit(X, Y)
+    monitors['grid_time'] = time.time() - start_time
 
-def test_model_on_dataset(name, params, data, n=50, n_folds=5):
+    results['best_params'] = cv_grid.best_params_
+    results['best_score'] = cv_grid.best_score_
 
-    if name == 'r2svm':
-        model = R2SVMLearner(**params)
-    elif name == 'r2elm':
-        model = R2ELMLearner(**params)
-    elif name[:3] == 'elm':
-        model = ELM(**params)
-    elif name == 'svm' :
-        model = SVC(**params)
+    if more:
+        monitors['fold_params'] = [ s[0] for s in cv_grid.grid_scores_ ]
+        monitors['mean_fold_scores'] = [s[1] for s in cv_grid.grid_scores_]
+        monitors['std_fold_scores'] = [np.std(s[2]) for s in cv_grid.grid_scores_]
 
+    monitors['best_std'] = [ np.std(s[2]) for s in cv_grid.grid_scores_ if s[0] == cv_grid.best_params_ ]
+
+    if store_clf:
+        monitors['clf'] = cv_grid
+
+    if logger is not None:
+        logger.info(results)
+        logger.info(monitors)
+
+    return experiment
+
+
+def k_fold(model, data, n_folds=10, seed=None, store_clf=False, logger=None):
+
+    assert hasattr(data, 'name')
+    assert hasattr(data, 'data')
+    assert hasattr(data, 'target')
+
+    results = {}
+    monitors = {}
+    config = {}
+    experiment = {"config": config, "results": results, "monitors": monitors}
+
+    config['n_folds'] = n_folds
+    config['seed'] = seed
+    config['store_clf'] = store_clf
+
+    monitors["acc_fold"] = []
+    monitors["train_time"] = []
+    monitors["test_time"] = []
+    monitors["clf"] = []
+
+    X, Y = data.data, data.target
+    folds = StratifiedKFold(y=Y, n_folds=n_folds, shuffle=True, random_state=seed)
+    for train_index, test_index in folds:
+        X_train, X_test, Y_train, Y_test = X[train_index], X[test_index], Y[train_index], Y[test_index]
+
+        train_start = time.time()
+        model.fit(X_train, Y_train)
+        monitors['train_time'].append(time.time() - train_start)
+
+        if store_clf :
+            monitors['clf'].append(model)
+
+        test_start = time.time()
+        Y_predicted = model.predict(X_test)
+        monitors['test_time'].append(time.time() - test_start)
+
+        monitors['acc_fold'].append(accuracy_score(Y_test, Y_predicted))
+
+    monitors['acc_fold'] = np.array(monitors['acc_fold'])
+    monitors['std'] = monitors['acc_fold'].std()
+
+    monitors['n_dim'] = data.n_dim
+    monitors['n_class'] = data.n_class
+    monitors['data_name'] = data.name
+
+    results["mean_acc"] = monitors["acc_fold"].mean()
+
+    if logger is not None :
+        logger.info(results)
+        logger.info(monitors)
+
+    return experiment
+
+
+def nk_folds(model, params, data, n=50, n_folds=10, n_jobs=4):
+
+    model.set_params(params)
     scores = []
     for _ in xrange(n) :
         data = shuffle_data(data)
-        scores.append(cross_val_score(estimator=model, X=data.data, y=data.target, scoring='accuracy', cv=n_folds, n_jobs=-1).mean())
+        scores.append(cross_val_score(estimator=model, X=data.data, y=data.target, scoring='accuracy', cv=n_folds, n_jobs=n_jobs).mean())
 
     return np.mean(scores), np.std(scores)
--- fit_r2elm.py
+++ fit_r2elm.py
@@ -1,184 +1,41 @@
+import pandas as pd
+import os
+import pickle
+from fit_models import grid_search
 from models import R2ELMLearner
-from misc.experiment_utils import get_exp_logger
-from misc.params import grid_config, fold_config
+from datetime import datetime
+from data_api import fetch_uci_datasets
+from misc.experiment_utils import get_logger
 
-from sklearn.grid_search import GridSearchCV
-from sklearn.cross_validation import KFold
-from sklearn.metrics import accuracy_score, confusion_matrix
 
-import time
-import numpy as np
+def main():
 
-# TODO: make a functions for running k-fold n-times
+    exp_name = 'R2ELM_grid_' + str(datetime.now().time())[:-7]
 
-def fit_r2elm_grid(data, config, logger=None):
-    """
-    Fits a GridSearchCV class from scikit-learn
-    :param data: dict-like with fields: name, data, target
-    :param config: dictionary with parameters specific for a grid search
-    :param logger: logger class
-    :return: dictionary Experiment with gird results
-    """
-    assert hasattr(data, 'name')
-    assert hasattr(data, 'data')
-    assert hasattr(data, 'target')
-    assert ['experiment_type', 'param_grid','scoring', 'cv', 'refit', 'store_clf'] <= config.keys()
-    assert config['experiment_type'] == 'grid'
-    ### Prepare result holders ###
-    results = {}
-    monitors = {}
+    params = {'h': [i for i in xrange(20,101,20)],
+              'beta': [0.05 * i for i in xrange(1,5)],
+              'fit_h': [None], # 'grid', 'random' or None
+              'depth': [i for i in xrange(1,11,2)],
+              'scale': [True, False],
+              'recurrent': [True, False],
+              'use_prev': [True, False],
+              'seed': [666]}
 
-    E = {"config": config, "results": results, "monitors": monitors}
-
+    small_datasets = fetch_uci_datasets(['iris', 'liver', 'heart'])
     model = R2ELMLearner()
-    cv_grid = GridSearchCV(model, param_grid=config['param_grid'], scoring=config['scoring'], n_jobs=-1, cv=config['cv'])
-
-    X = data.data
-    Y = data.target
-
-    start_time = time.time()
-    cv_grid.fit(X, Y)
-    monitors['grid_time'] = time.time() - start_time
-
-    results['best_params'] = cv_grid.best_params_
-    results['best_score'] = cv_grid.best_score_
-    if config['refit'] :
-        results['best_cls'] = R2ELMLearner(**cv_grid.best_params_).fit(X,Y)
-    else :
-        results['best_cls'] = cv_grid.best_estimator_
-
-    monitors['fold_params'] = [ s[0] for s in cv_grid.grid_scores_ ]
-    monitors['mean_fold_scores'] = [s[1] for s in cv_grid.grid_scores_]
-    monitors['std_fold_scores'] = [np.std(s[2]) for s in cv_grid.grid_scores_]
-    monitors['best_std'] = [ np.std(s[2]) for s in cv_grid.grid_scores_ if s[1] == cv_grid.best_score_ ]
-    monitors['data_name'] = data.name
-
-    if config['store_clf'] :
-        monitors['clf'] = cv_grid
-
-    if logger is not None :
-        logger.info(results)
-        logger.info(monitors)
-
-    return E
-
-def fit_r2elm(data, config, logger=None) :
-    """
-    Fits R2SVMLearner class on
-    :param data: dict-like with fields: name, data, target
-    :param config: dictionary with parameters specific for a k-fold fitting
-    :param logger: logger class
-    :return: dictionary Experiment with cross validation results results
-    """
-    assert hasattr(data, 'name')
-    assert hasattr(data, 'data')
-    assert hasattr(data, 'target')
-    assert ['experiment_type', 'n_folds', 'fold_seed', 'params', 'store_clf'] <= config.keys()
-    assert config['experiment_type'] == 'k-fold'
-
-    ### Prepare result holders ###b
+    logger = get_logger(exp_name, to_file=False)
     results = {}
-    monitors = {}
-    E = {"config": config, "results": results, "monitors": monitors}
 
-    monitors["acc_fold"] = []
-    monitors["train_time"] = []
-    monitors["test_time"] = []
-    monitors["cm"] = [] # confusion matrix
-    monitors["clf"] = []
+    for data in small_datasets:
+        exp = grid_search(model, data, params, logger=logger)
+        results[data.name] = pd.DataFrame.from_dict(exp['monitors'].update(exp['results']))
+        print data.name + " done!"
 
-    X, Y = data.data, data.target
-    folds = KFold(n=X.shape[0], n_folds=config['n_folds'], shuffle=True, random_state=config['fold_seed'])
-    for train_index, test_index in folds :
-        X_train, X_test, Y_train, Y_test = X[train_index], X[test_index], Y[train_index], Y[test_index]
+    f = open(os.path.join('./cache/' + exp_name + '.pkl'), 'wb')
+    pickle.dump(results, f)
+    f.close()
 
-        model = R2ELMLearner(**config['params'])
-        train_start = time.time()
-        model.fit(X_train, Y_train)
-        monitors['train_time'].append(time.time() - train_start)
-
-        if config['store_clf'] :
-            monitors['clf'].append(model)
-
-        test_start = time.time()
-        Y_predicted = model.predict(X_test)
-        monitors['test_time'].append(time.time() - test_start)
-
-        monitors['acc_fold'].append(accuracy_score(Y_test, Y_predicted))
-        monitors['cm'].append(confusion_matrix(Y_test, Y_predicted))
-
-    monitors['acc_fold'] = np.array(monitors["acc_fold"])
-    monitors['std'] = monitors['acc_fold'].std()
-    monitors['n_dim'] = data.n_dim
-    monitors['n_class'] = data.n_class
-    monitors['data_name'] = data.name
-
-    results['mean_acc'] = monitors['acc_fold'].mean()
-    monitors["mean_acc"] = monitors["acc_fold"].mean()
-
-    if logger is not None :
-        logger.info(results)
-        logger.info(monitors)
-
-    return E
+if __name__ == '__main__':
+    main()
 
 
-def fit_r2elm_grid_default(data):
-    """Fits a exhausting grid search with default parameters on a data set, return dictionary with results"""
-
-    config = {'experiment_type': 'grid',
-                  'param_grid': default_r2elm_grid_parameters(),
-                  'n_fold': 3,
-                  'scoring': 'accuracy',
-                  'store_clf': False,
-                  'seed': None,
-                  'experiment_name': 'R2ELM default grid experiment on ' + data.name}
-
-    assert hasattr(data, 'name')
-    assert hasattr(data, 'data')
-    assert hasattr(data, 'target')
-
-    return  fit_r2elm_grid(config, data.data, data.target)
-
-def fit_r2elm_on_dataset(data, param_grid_in=None, grid_config_in=None, fold_config_in=None, to_file=False):
-
-    param_grid = default_r2elm_grid_parameters()
-    if param_grid_in is not None :
-        param_grid.update(param_grid_in)
-
-    fold_seed = np.random.randint(0, np.iinfo(np.int32).max)
-    random_state = np.random.RandomState(fold_seed)
-
-    grid_config['experiment_name'] = 'R2ELM_grid_search_on_' + data.name + str(time.time())
-    grid_config['fold_seed'] = fold_seed
-    grid_config['cv'] = KFold(n=data.data.shape[0], shuffle=True, n_folds=3, random_state=random_state)
-    grid_config['param_grid'] = param_grid
-
-    if grid_config_in is not None :
-        grid_config.update(grid_config_in)
-
-    logger = get_exp_logger(grid_config, to_file=to_file)
-
-    E_grid = fit_r2elm_grid(data, grid_config, logger)
-    params = E_grid['results']['best_params']
-
-    fold_config['experiment_name'] = 'R2ELM_k-fold_on_' + data.name + str(time.time())
-    fold_config['fold_seed'] =  E_grid['config']['fold_seed']
-    fold_config['params'] = params
-
-    if fold_config_in is not None :
-        fold_config.update(fold_config_in)
-
-    logger = get_exp_logger(fold_config, to_file=to_file)
-
-    return E_grid, fit_r2elm(data, fold_config, logger)
-
-def default_r2elm_grid_parameters() :
-    return { 'h': [i for i in xrange(10,60,10)],
-             'beta': [0.05 * i for i in xrange(0,7)],
-             'fit_h': None, # 'grid', 'random' or None
-             'depth': [5],
-             'scale': [True],
-             'recurrent': [True],
-             'use_prev': [True],
-             'seed': [None]}
